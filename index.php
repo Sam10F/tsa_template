61bec1231d662a0ead68e1d6f596252d4e0cedc3
samuel10 <samueldf7994@gmail.com>
2020-01-13 00:19:05 +0200

    Update the code in the new mac!!

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php 
 
-$con = new mysqli("127.0.0.1", "root", "samuel88", "samuel10f");
-$message = $con->query("SELECT nickName FROM testTable")->fetch_object()->nickName;
+$con = new mysqli("127.0.0.1", "root", "samuel88", "MVC");
+$message = $con->query("SELECT nickname FROM users")->fetch_object()->nickname;
 $con->close();
 echo "$message <br/>";
-echo "Hello From Sites Folder!";
+echo "Hello From Sites Folder with the user" . $message . "!";
 
 
 	// la variable controller guarda el nombre del controlador y action guarda la acción por ejemplo registrar 
 	//si la variable controller y action son pasadas por la url desde layout.php entran en el if
-	if (isset($_GET['controller'])&&isset($_GET['action'])) {
+	if (isset($_GET['controller']) && isset($_GET['action'])) {
 		$controller=$_GET['controller'];
 		$action=$_GET['action'];
 	} else {
